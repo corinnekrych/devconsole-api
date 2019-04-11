6ec63107fa8e76aa4a37409b663649df22e1936d
Corinne Krych <corinnekrych@gmail.com>
2019-04-11 14:16:17 +0200

    add phase in status to inform on build, deployment progress

--- pkg/apis/devconsole/v1alpha1/component_types.go
+++ pkg/apis/devconsole/v1alpha1/component_types.go
@@ -4,7 +4,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// ComponentSpec defines the desired state of Component.
+// ComponentSpec defines the desired state of Component
 // +k8s:openapi-gen=true
 type ComponentSpec struct {
 	// Container image use to build (nodejs, golang etc..)
@@ -17,15 +17,23 @@ type ComponentSpec struct {
 	Exposed bool `json:"exposed,omitempty"`
 }
 
-// ComponentStatus defines the observed state of Component.
+// ComponentStatus defines the observed state of Component
 // +k8s:openapi-gen=true
 type ComponentStatus struct {
-	RevNumber string
+	RevNumber string `json:"revNumber,omitempty"`
+	Phase     string `json:"phase,omitempty"`
 }
+
+const (
+	// PhaseDeploying indicate component is under deployment
+	PhaseDeploying string = "Deploying"
+	// PhaseDeployed indicate component is deployed
+	PhaseDeployed string = "Deployed"
+)
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Component is the Schema for the components API.
+// Component is the Schema for the components API
 // +k8s:openapi-gen=true
 type Component struct {
 	metav1.TypeMeta   `json:",inline"`
@@ -42,7 +50,7 @@ func (c *Component) GetName() string {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// ComponentList contains a list of Component.
+// ComponentList contains a list of Component
 type ComponentList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
